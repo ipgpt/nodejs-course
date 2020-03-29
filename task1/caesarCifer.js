c2d448ccdc47c1c724c05e5d99178b156cc3bdbb
Petro Fedorenko <petewaitforletters@gmail.com>
2020-03-29 20:05:51 +0300

    fix: remove unnecessary variables and replace some let to const

--- task1/caesarCifer.js
+++ task1/caesarCifer.js
@@ -1,68 +1,59 @@
 const codeUpperLetterA = 65;
-const codeUpperLetterB = 66;
-const codeUpperLetterC = 67;
-const codeUpperLetterX = 88;
-const codeUpperLetterY = 89;
 const codeUpperLetterZ = 90;
 
 const codeLowerLetterA = 97;
-const codeLowerLetterB = 98;
-const codeLowerLetterC = 99;
-const codeLowerLetterX = 120;
-const codeLowerLetterY = 121;
 const codeLowerLetterZ = 122;
 
 const turnToLetter = charCode => String.fromCharCode(charCode);
 
-const caesarCifer = (text, shiftNumber, action) => {
+const caesarCipher = (text, shiftNumber, action) => {
   let changedText = '';
   for (let i = 0; i < text.length; i++) {
-    let char = text[i];
-    let charCode = char.charCodeAt();
+    const char = text[i];
+    const charCode = char.charCodeAt();
     if (action === 'encode') {
-      let shiftedCharCode = charCode + shiftNumber;
-      if (charCode >= codeUpperLetterA &&
-        charCode <= codeUpperLetterZ) {
-          if (shiftedCharCode > codeUpperLetterZ) {
-            changedText += turnToLetter(shiftedCharCode - 
-              codeUpperLetterZ + codeUpperLetterA - 1);
-          } else {
-            changedText += turnToLetter(shiftedCharCode);
-          }
-      } else if (charCode >= codeLowerLetterA &&
-        charCode <= codeLowerLetterZ) {
-          if (shiftedCharCode > codeLowerLetterZ) {
-            changedText += turnToLetter(shiftedCharCode - 
-              codeLowerLetterZ + codeLowerLetterA - 1);
-          } else {
-            changedText += turnToLetter(shiftedCharCode);
-          }
+      const shiftedCharCode = charCode + shiftNumber;
+      if (charCode >= codeUpperLetterA && charCode <= codeUpperLetterZ) {
+        if (shiftedCharCode > codeUpperLetterZ) {
+          changedText += turnToLetter(
+            shiftedCharCode - codeUpperLetterZ + codeUpperLetterA - 1
+          );
+        } else {
+          changedText += turnToLetter(shiftedCharCode);
+        }
+      } else if (charCode >= codeLowerLetterA && charCode <= codeLowerLetterZ) {
+        if (shiftedCharCode > codeLowerLetterZ) {
+          changedText += turnToLetter(
+            shiftedCharCode - codeLowerLetterZ + codeLowerLetterA - 1
+          );
+        } else {
+          changedText += turnToLetter(shiftedCharCode);
+        }
       } else {
         changedText += char;
       }
     } else if (action === 'decode') {
-      let shiftedCharCode = charCode - shiftNumber;
-      if (charCode >= codeUpperLetterA &&
-        charCode <= codeUpperLetterZ) {
-          if (shiftedCharCode < codeUpperLetterA) {
-            changedText += turnToLetter(codeUpperLetterZ - 
-              (codeUpperLetterA - shiftedCharCode) + 1);
-          } else {
-            changedText += turnToLetter(shiftedCharCode);
-          }
-      } else if (charCode >= codeLowerLetterA &&
-        charCode <= codeLowerLetterZ) {
-          if (shiftedCharCode < codeLowerLetterA) {
-            changedText += turnToLetter(codeLowerLetterZ - 
-              (codeLowerLetterA - shiftedCharCode) + 1);
-          } else {
-            changedText += turnToLetter(shiftedCharCode);
-          }
+      const shiftedCharCode = charCode - shiftNumber;
+      if (charCode >= codeUpperLetterA && charCode <= codeUpperLetterZ) {
+        if (shiftedCharCode < codeUpperLetterA) {
+          changedText += turnToLetter(
+            codeUpperLetterZ - (codeUpperLetterA - shiftedCharCode) + 1
+          );
+        } else {
+          changedText += turnToLetter(shiftedCharCode);
+        }
+      } else if (charCode >= codeLowerLetterA && charCode <= codeLowerLetterZ) {
+        if (shiftedCharCode < codeLowerLetterA) {
+          changedText += turnToLetter(
+            codeLowerLetterZ - (codeLowerLetterA - shiftedCharCode) + 1
+          );
+        } else {
+          changedText += turnToLetter(shiftedCharCode);
+        }
       } else {
         changedText += char;
       }
-    }
-    else {
+    } else {
       return text;
     }
   }
@@ -70,7 +61,7 @@ const caesarCifer = (text, shiftNumber, action) => {
 };
 
 // simple test
-let test1 = caesarCifer('Hello World!', 7, 'encode');
+const test1 = caesarCipher('Hello World!', 7, 'encode');
 console.log(test1); // Olssv Dvysk!
-let test2 = caesarCifer(test1, 7, 'decode');
+const test2 = caesarCipher(test1, 7, 'decode');
 console.log(test2); // Hello World!
